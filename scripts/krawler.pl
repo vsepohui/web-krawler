9ca7738c355a63bfdcecd6d378f658b41d9e72c8
Ivan Trunaev <trunaev.ivan@icloud.com>
2023-12-12 19:14:32 +0500

    Update

--- scripts/krawler.pl
+++ scripts/krawler.pl
@@ -7,9 +7,12 @@ use AnyEvent::HTTP;
 use Sub::Daemon;
 use Krawler::Config;
 use URL::Search;
+use HTML::LinkExtor;
 use Redis;
 use AnyEvent::Redis;
 use List::Uniq qw/uniq/;
+use URI;
+use utf8;
 
 use constant PORT_START_FROM => 3000;
 use constant REDIS_PAGE_PREF => 'web-krawler:page:';
@@ -50,13 +53,27 @@ sub help {
 my %waits = ();
 my $cnt = 0;
 
+my %wait2 = ();
+my %bad_domain = ();
+
+
+
 sub work {
+	my $cv = shift;
 	$cnt ++;
-	$redis->lpop(REDIS_QUEUE(), sub {
-		my ($url) = (@_);
+
+	warn "Startig BLPOP";
+	$wait2{$cnt} = $redis->blpop(REDIS_QUEUE(), 300, sub {
+		delete $wait2{$cnt};
+		my ($p) = @_;
+		my ($q, $url) = @$p;;
+		
+		use Data::Dumper;
+		warn "URL => ".Dumper \@_;
+					
 		unless ($url) {
 			warn "No url";
-			sleep 1;
+
 			return;
 		}
 
@@ -65,25 +82,66 @@ sub work {
 		$waits{$url} = http_request(
 			GET => $url, 
 			headers => { "user-agent" => "MySearchClient 1.0" },
+			timeout => 10,
 			sub {
 				delete $waits{$url};
-				work() if $cnt < 8000;
 				$redis2->hdel(REDIS_LOCK(),$url);
+				my $uri = URI->new($url);
+				my $host = $uri->host;
+				my $host2 = $host;
+				my ($proto) = $url =~ /^(\w+):\/\//;
+				my $url_base = $url;
+						
 				warn "Fetched url $url";
 				my $data = shift;
 				my $headers = shift;
 				use Data::Dumper;
 				warn Dumper ($headers);
+
+				if ($headers->{Status} eq '595') {
+					$bad_domain{$uri->host}++;
+				}
+
 				my $type = $headers->{'content-type'};
 				$type =~ s/;.+$//;
-				return unless $type ~~ ['text/html', 'text/plain'];
+				unless ($type ~~ ['text/html', 'text/plain']) {
+					$cv->send unless keys %wait2;
+					return;
+				}
 				if (length ($data) < 1_00_0000) {
 					warn "Data $url => " . $data;
 					$redis2->set(REDIS_PAGE_PREF().$url => $data);
 					$redis2->set(REDIS_PAGE_CTIME() => time());
 					
+					my @as;
+					sub callback {
+						my($tag, %attr) = @_;
+						return if $tag ne 'a';  # we only look closer at <img ...>
+						push(@as, values %attr);
+					}
+					
+					my $p = HTML::LinkExtor->new(\&callback);
+					
 					my @urls = uniq (URL::Search::extract_urls($data));
-					for my $url (@urls) {
+					$p->parse($data);
+					warn Dumper \@as;
+					for my $url (map {$_->{href}} @as) {
+						if ($url =~ /^\//) {
+							$url = "$proto:\/\/$host2" . $url;
+						} elsif ($url =~ /^\https?:\/\// ) {
+							1;
+						}  elsif ($url =~ /^\/\// ) {
+							$url = $proto . ':' . $url;
+						} elsif ($url =~ /^\[.]/ ) {
+							$url = $url_base . $url;
+						}
+						my $uri = URI->new($url);
+						my $host = $uri->host;
+							
+						
+						next if ($bad_domain{$host} > 10);
+						next if $url =~ /^https?\:\/\/www\.w3\.org\//;
+						next if $url =~ /\.(js|css|png|jpeg|jpg|gif|mp3|webp|font|ttf)\??/;
 						warn "Found url: $url";
 						unless ($redis2->get(REDIS_PAGE_PREF.$url) && $redis2->hget(REDIS_LOCK(),$url)) {
 							$redis2->hset(REDIS_LOCK(), $url => 1);
@@ -92,11 +150,10 @@ sub work {
 						}
 					}
 				}
+				$cv->send unless keys %wait2;
 			},
 		);
-		work() if $cnt < 8000;
 	});
-	#
 }
 
 
@@ -126,9 +183,9 @@ sub worker {
 			$SIG{$_} = sub { $cv->send } for qw( TERM INT );
 			$SIG{'HUP'} = sub {$cv->send};
 
-			work();
+			work($cv) for 1..100;
 			
-			$cv->wait;
+			$cv->recv;
 		},
 	);
 }
